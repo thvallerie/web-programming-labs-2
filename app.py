17aed9264718ed731115f72cd7f2d601af81f644
Valerie Booth <lera.but.03@bk.ru>
2023-09-13 18:37:39 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start ():
-    return "web-сервер на flask"
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Бут Валерия Сергеевна, Лабораторная 1 </title>
+      </head>
+      <body>
+        <header>
+          НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+          &copy; Бут Валерия, ФБИ-13, 3 курс, 2023
+        </footer>
+      </body>
+    </html>
+"""
