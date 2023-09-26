fbb62b64a96fe99cc6b0ed9baa1f59f0cd044040
Valerie Booth <lera.but.03@bk.ru>
2023-09-26 16:34:22 +0700

    Заготовка шаблона

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 def start ():
   return redirect("/menu", code=302)
+
+@app.route('/lab2/example')
+def example():
+  return render_template('example.html')
 
 @app.route("/menu")
 def menu ():
