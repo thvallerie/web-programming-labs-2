3a6ab1037fac8eeea4fdd345bdd1d9803d541737
Valerie Booth <lera.but.03@bk.ru>
2023-09-27 12:30:33 +0700

    Внесла последние коммиты: фрукты,книги

--- app.py
+++ app.py
@@ -8,11 +8,32 @@ def start ():
 
 @app.route('/lab2/example')
 def example():
-  name = 'Бут Валерия'
-  lab_number = '№2'
-  group = 'ФБИ-13'
-  course = '3 курс'
-  return render_template('example.html')
+    name, lab_num, group, course ='Бут Валерия', 2, 'ФБИ-13', 3
+    fruits = [
+      {'name': 'яблочки', 'price': 100},
+      {'name': 'груши', 'price': 120},
+      {'name': 'апельсинчики', 'price': 80},
+      {'name': 'мандаринчики', 'price': 95},
+      {'name': 'манго', 'price': 321},
+    ]
+
+    return render_template('example.html',
+                         name=name, lab_num=lab_num, group=group,
+                         course=course, fruits=fruits, books=books)
+
+books = [
+        {'book' : 'Мастер и Маргарита', 'author' : 'Михаил Булгаков', 'zhanr' : 'Любовный роман', 'str' : 480},
+        {'book' : 'Убийство в восточном экспрессе', 'author' : 'Агата Кристи', 'zhanr' : 'Детектив', 'str' : 317},
+        {'book' : 'Код Да Винчи', 'author' : 'Дэн Браун', 'zhanr' : 'Детектив, Триллер', 'str' : 544},
+        {'book' : 'Ганнибал', 'author' : 'Томас Харрис', 'zhanr' : ' Психологический роман ужасов', 'str' : 580},
+        {'book' : 'Финансист', 'author' : 'Теодор Драйзер', 'zhanr' : 'Роман', 'str' : 576},
+        {'book' : 'Дракула', 'author' : 'Брэм Стокер', 'zhanr' : 'Роман', 'str' : 500},
+        {'book' : 'Приключения Шерлока Холмса', 'author' : 'Артур Конан-Дойл', 'zhanr' : 'Детектив', 'str' : 2300},
+        {'book' : 'Божественная комедия', 'author' : 'Данте Алегьери', 'zhanr' : 'Поэма', 'str' : 548},
+        {'book' : 'Не тупи', 'author' : 'Джон Синсеро', 'zhanr' : 'Зарубежная психология', 'str' : 70},
+        {'book' : 'Дюна', 'author' : 'Фрэнк Герберт', 'zhanr' : 'Фантастика', 'str' : 768},
+        {'book' : 'Фауст', 'author' : 'Иоганн Гёте', 'zhanr' : 'Филосовская драма', 'str' : 350},
+    ]
 
 @app.route("/menu")
 def menu ():
