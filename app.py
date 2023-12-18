2604a4ad4bd2e302bdbdfc7b6530307579a7e72b
Valerie Booth <lera.but.03@bk.ru>
2023-12-18 14:57:58 +0700

    правки по 8 лабе

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
+from lab8 import lab8
 from Db import db
 from flask_login import LoginManager
 from Db.models import users
@@ -18,7 +19,7 @@ app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
-
+app.register_blueprint(lab8)
 
 app.secret_key = 'SuPeRSeCrEtKeY_1234567890'
 user_db = 'valerie_knowledge_base'
---
+++ lab8.py
@@ -0,0 +1,52 @@
+from flask import Blueprint, render_template, request, abort, jsonify
+import datetime
+
+lab8 = Blueprint('lab8', __name__)
+
+@lab8.route('/lab8/')
+def main():
+    return render_template('lab8/index.html')
+
+courses = [
+    {'name': 'c++', 'videos': 3, 'price': 3000, 'data': None},
+    {'name': 'basic', 'videos': 30, 'price': 0, 'data': None},
+    {'name': 'c#', 'videos': 8, 'data': None}
+]
+
+@lab8.route('/lab8/api/courses/', methods = ['GET'])
+def get_courses():
+    return jsonify(courses)
+
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=['GET'])
+def get_course(course_num):
+    if course_num < 0 or course_num >= len(courses)-1:
+        abort(404)
+    else:
+        return courses[course_num]
+
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=['DELETE'])
+def del_course(course_num):
+    if course_num < 0 or course_num >= len(courses)-1:
+        abort(404)
+    else:
+        del courses[course_num]
+    return '', 204
+
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=['PUT'])
+def put_course(course_num):
+    course = request.get_json()
+    #saved_course_data = courses[course_num]['data'] 
+    courses[course_num] = course 
+    #course['data'] = saved_course_data
+    if course_num < 0 or course_num >= len(courses)-1:
+        abort(404)
+    else:
+        return courses[course_num]
+
+
+@lab8.route('/lab8/api/courses/', methods = ['POST'])
+def add_course():
+    course = request.get_json()
+    course['data'] = datetime.datetime.now()
+    courses.append(course)
+    return {'num': len(courses)-1}
